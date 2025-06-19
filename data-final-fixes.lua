cdcc0aa23a56e4f7ec5ff9fb8365b6d743386357
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2025-06-19 12:11:05 -0700

    Bugfixes
    Read settings values correctly (need .value)
    
    Don't add module slots to structures without an allowed_effects value.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -4,32 +4,37 @@ local function apply_qef_to_type(prototype_name)
             goto continue
         end
 
+        if obj["allowed_effects"] ~= nil and table_size(obj["allowed_effects"]) == 0 then
+            -- Not sure if I want to change this in 1.2.0, possibly by adding a setting to force override this.
+            goto continue
+        end
+
         data.raw[prototype_name][name]["quality_affects_module_slots"] = true
 
         ::continue::
     end
 end
 
-if settings.startup["qef-affects-assembling-machines"] then
+if settings.startup["qef-affects-assembling-machines"].value then
     apply_qef_to_type("assembling-machine")
 end
 
-if settings.startup["qef-affects-furnaces"] then
+if settings.startup["qef-affects-furnaces"].value then
     apply_qef_to_type("furnace")
 end
 
-if settings.startup["qef-affects-silos"] then
+if settings.startup["qef-affects-rocket-silos"].value then
     apply_qef_to_type("rocket-silo")
 end
 
-if settings.startup["qef-affects-beacons"] then
+if settings.startup["qef-affects-beacons"].value then
     apply_qef_to_type("beacon")
 end
 
-if settings.startup["qef-affects-mining-drills"] then
+if settings.startup["qef-affects-mining-drills"].value then
     apply_qef_to_type("mining-drill")
 end
 
-if settings.startup["qef-affects-labs"] then
+if settings.startup["qef-affects-labs"].value then
     apply_qef_to_type("lab")
 end
