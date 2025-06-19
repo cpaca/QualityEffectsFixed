3fca840a8a6e3a4ba685f294e01301e81f443961
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2025-06-19 08:49:33 -0700

    feat: Separate toggles for furnaces and crafters

--- settings.lua
+++ settings.lua
@@ -1,25 +1,31 @@
 data:extend({
     {
         type = "bool-setting",
-        name = "qef-affects-assembling-machines",
+        name = "qef_affects_assembling_machines",
         setting_type = "startup",
         default_value = true
     }, 
     {
         type = "bool-setting",
-        name = "qef-affects-beacons",
+        name = "qef_affects_furnaces",
         setting_type = "startup",
         default_value = false
     }, 
     {
         type = "bool-setting",
-        name = "qef-affects-mining-drills",
+        name = "qef_affects_beacons",
         setting_type = "startup",
         default_value = false
     }, 
     {
         type = "bool-setting",
-        name = "qef-affects-labs",
+        name = "qef_affects_mining_drills",
+        setting_type = "startup",
+        default_value = false
+    }, 
+    {
+        type = "bool-setting",
+        name = "qef_affects_labs",
         setting_type = "startup",
         default_value = false
     }
