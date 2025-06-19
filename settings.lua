db45c838c4d6b7fa71e2227e555197c08533594d
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2025-06-19 08:16:27 -0700

    feat: Add toggle settings for QEF features

---
+++ settings.lua
@@ -0,0 +1,26 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "qef-affects-assembling-machines",
+        setting_type = "startup",
+        default_value = true
+    }, 
+    {
+        type = "bool-setting",
+        name = "qef-affects-beacons",
+        setting_type = "startup",
+        default_value = false
+    }, 
+    {
+        type = "bool-setting",
+        name = "qef-affects-mining-drills",
+        setting_type = "startup",
+        default_value = false
+    }, 
+    {
+        type = "bool-setting",
+        name = "qef-affects-labs",
+        setting_type = "startup",
+        default_value = false
+    }
+})
